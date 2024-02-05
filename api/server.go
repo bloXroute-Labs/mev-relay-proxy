36093d56ac76608f66b08aa877da139d2ed94563
Bhakiyaraj Kalimuthu <bhakiyaraj.kalimuthu@bloxroute.com>
2024-02-05 11:48:37 +0100

    log relay and relay proxy error while responding error (#45)

--- api/server.go
+++ api/server.go
@@ -280,19 +280,36 @@ func respondError(method string, w http.ResponseWriter, err error, log *zap.Logg
 	}
 	resp, ok := err.(*ErrorResp)
 	if !ok {
-		log.With(zap.String("req_id", resp.BlxrMessage.reqID), zap.String("blxr_message", resp.BlxrMessage.msg), zap.String("client_ip", resp.BlxrMessage.clientIP), zap.String("resp_message", resp.Message), zap.Int("resp_code", resp.Code)).Error("failed to typecast error response", zap.String("metaData", meta))
+		log.With(zap.String("req_id", resp.BlxrMessage.reqID),
+			zap.String("client_ip", resp.BlxrMessage.clientIP),
+			zap.String("blxr_message", resp.BlxrMessage.msg),
+			zap.String("relay_message", resp.BlxrMessage.relayMsg),
+			zap.String("proxy_message", resp.BlxrMessage.proxyMsg),
+			zap.String("resp_message", resp.Message),
+			zap.Int("resp_code", resp.Code)).Error("failed to typecast error response", zap.String("metaData", meta))
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		return
 	}
 
 	w.WriteHeader(resp.Code)
-	log.With(zap.String("req_id", resp.BlxrMessage.reqID), zap.String("blxr_message", resp.BlxrMessage.msg), zap.String("client_ip", resp.BlxrMessage.clientIP), zap.String("resp_message", resp.Message), zap.Int("resp_code", resp.Code)).Error(fmt.Sprintf("%s failed", method), zap.String("metaData", meta))
+	log.With(zap.String("req_id", resp.BlxrMessage.reqID),
+		zap.String("client_ip", resp.BlxrMessage.clientIP),
+		zap.String("blxr_message", resp.BlxrMessage.msg),
+		zap.String("relay_message", resp.BlxrMessage.relayMsg),
+		zap.String("proxy_message", resp.BlxrMessage.proxyMsg),
+		zap.String("resp_message", resp.Message),
+		zap.Int("resp_code", resp.Code)).Error(fmt.Sprintf("%s failed", method), zap.String("metaData", meta))
 	if resp.Message != "" && resp.Code != http.StatusNoContent { // HTTP status "No Content" implies that no message body should be included in the response.
 		if err := json.NewEncoder(w).Encode(resp); err != nil {
-			log.With(zap.String("req_id", resp.BlxrMessage.reqID), zap.String("blxr_message", resp.BlxrMessage.msg), zap.String("client_ip", resp.BlxrMessage.clientIP), zap.String("resp_message", resp.Message), zap.Int("resp_code", resp.Code)).Error("couldn't write error response", zap.Error(err), zap.String("metaData", meta))
+			log.With(zap.String("req_id", resp.BlxrMessage.reqID),
+				zap.String("blxr_message", resp.BlxrMessage.msg),
+				zap.String("client_ip", resp.BlxrMessage.clientIP),
+				zap.String("relay_message", resp.BlxrMessage.relayMsg),
+				zap.String("proxy_message", resp.BlxrMessage.proxyMsg),
+				zap.String("resp_message", resp.Message),
+				zap.Int("resp_code", resp.Code)).Error("couldn't write error response", zap.Error(err), zap.String("metaData", meta))
 			_, _ = w.Write([]byte(``))
 			return
 		}
-		return
 	}
 }
