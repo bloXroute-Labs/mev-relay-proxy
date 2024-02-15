474395337f2b14d42b5897d134497bfa3a66250c
BenHenryHunter <benhenryhunter@gmail.com>
2024-02-15 17:18:46 -0500

    Add build args

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ export CGO_CFLAGS
 COMMIT=$(git rev-parse --short HEAD)
 TAG=${1:-dev}-$COMMIT
 IMAGE=bloxroute/mev-relay-proxy-internal:$TAG
+SECRET_TOKEN=${2:4nDpR2sVxYz1BtU6wFqGhJkLp3Tm5ZoX}
 
 TOKEN=${2:$GITHUB_TOKEN}
 
@@ -13,7 +14,7 @@ if [ "$#" -eq 1 ]; then
 fi
 
 echo "Go build..."
-GOOS=linux GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux-musl-g++  go build -o mev-relay-proxy-internal ./cmd/mev-relay-proxy
+GOOS=linux GOARCH=amd64 CGO_ENABLED=1 CC=x86_64-linux-musl-gcc  CXX=x86_64-linux-musl-g++  go build -ldflags "-s -X main._BuildVersion=${TAG} -X main._SecretToken=${SECRET_TOKEN}" -o mev-relay-proxy-internal ./cmd/mev-relay-proxy
 echo "Building container... $IMAGE With tag... $TAG"
 docker build . -f Dockerfile --rm=true --platform linux/x86_64 --build-arg token=$TOKEN -t $IMAGE
 docker tag bloxroute/mev-relay-proxy-internal:$TAG bloxroute/mev-relay-proxy-internal:$TAG
